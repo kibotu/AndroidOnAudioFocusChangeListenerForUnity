ceb016e9d72e0dd4a4323a2275310252ddc5266d
Jan Rabe <jan.rabe@exozet.com>
2014-12-03 17:16:41 +0100

    Exchanged default UnityPlayerNativeActivity class in order to get hold of mUnityPlayer so I can access fmod directly.

--- Android/Source/src/net/kibotu/audiolistener/ManualUnityActivity.java
+++ Android/Source/src/net/kibotu/audiolistener/ManualUnityActivity.java
@@ -1,16 +1,18 @@
 package net.kibotu.audiolistener;
 
 import android.os.Bundle;
+import com.unity3d.player.UnityPlayer;
 import com.unity3d.player.UnityPlayerNativeActivity;
 import org.jetbrains.annotations.Nullable;
 
 public class ManualUnityActivity extends UnityPlayerNativeActivity {
 
-
+    public static UnityPlayer unityPlayer;
 
     protected void onCreate(@Nullable final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        // make mUnityPlayer accessible in static context
+        unityPlayer = mUnityPlayer;
     }
 }
